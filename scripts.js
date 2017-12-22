b99815ee329f9f92b0d7549e0e464e6f5195bb4b
Alterux <alterux@gmail.com>
2017-12-22 23:18:20 +0100

    made circles clickable

--- scripts.js
+++ scripts.js
@@ -3,17 +3,48 @@ window.onload = function() {
   countDown();
 
   // Update countdown on a 1 sec interval.
-  let x = setInterval(countDown(), 1000);
+  setInterval("countDown()", 1000);
 }
 
 // Are the date circles expanded?
-let dateExpanded = 0;
+let monthsExpanded = 0;
+
+function monthsExpand() {
+  if (monthsExpanded === 0) {
+    document.getElementById('monthsExpanded').style.display = "block"
+    monthsExpanded = 1;
+  } else {
+    document.getElementById('monthsExpanded').style.display = "none"
+    monthsExpanded = 0;
+  }
+}
+
+let weeksExpanded = 0;
+
+function weeksExpand() {
+  if (weeksExpanded === 0) {
+    document.getElementById('weeksExpanded').style.display = "block"
+    weeksExpanded = 1;
+  } else {
+    document.getElementById('weeksExpanded').style.display = "none"
+    weeksExpanded = 0;
+  }
+}
+
+let daysExpanded = 0;
+
+function daysExpand() {
+  if (daysExpanded === 0) {
+    document.getElementById('daysExpanded').style.display = "block"
+    daysExpanded = 1;
+  } else {
+    document.getElementById('daysExpanded').style.display = "none"
+    daysExpanded = 0;
+  }
+}
 
 // Countdown date.
 let countDownDate = new Date("Jun 10, 2018 01:35:00").getTime();
-
-// let color = document.getElementById('monthsCircle').style.backgroundColor;
-
 
 function countDown() {
 
@@ -32,11 +63,11 @@ function countDown() {
   let months = Math.floor(days / 30);
   let weeks = Math.floor(days / 7);
 
-  if (dateExpanded === 1) {
-
     // Display months.
     document.getElementById('months').innerHTML =
-    months + " Months<br>" +
+    months + " Months<br>";
+
+    document.getElementById('monthsExpanded').innerHTML =
     (days % 30 - 1) + " Days<br>" +
     (hours + 1) + " Hours<br>" +
     minutes + " Minutes<br>" +
@@ -44,7 +75,9 @@ function countDown() {
 
     // Display weeks.
     document.getElementById('weeks').innerHTML =
-    weeks + " Weeks<br>" +
+    weeks + " Weeks<br>";
+
+    document.getElementById('weeksExpanded').innerHTML =
     (days % 7 + 1) + " Days<br>" +
     (hours + 1) + " Hours<br>" +
     minutes + " Minutes<br>" +
@@ -52,7 +85,9 @@ function countDown() {
 
     // Display days.
     document.getElementById('days').innerHTML =
-    days + " Days<br>" +
+    days + " Days<br>";
+
+    document.getElementById('daysExpanded').innerHTML =
     (hours + 1) + " Hours<br>" +
     minutes + " Minutes<br>" +
     seconds + " Seconds";
@@ -63,28 +98,5 @@ function countDown() {
       document.getElementById('countDown').innerHTML =
       (-days-1) + "d " + (-hours-1) + "h " + (-minutes-1) + "m " + (-seconds-1) + "s " + "<br>" +
       "Since Israel";
-    }
-
-  } else {
-
-    // Display months.
-    document.getElementById('months').innerHTML =
-    months + " Months<br>";
-
-    // Display weeks.
-    document.getElementById('weeks').innerHTML =
-    weeks + " Weeks<br>";
-
-    // Display days.
-    document.getElementById('days').innerHTML =
-    days + " Days<br>";
-
-    // After date reached.
-    if (distance < 0) {
-      // clearInterval(x);
-      document.getElementById('countDown').innerHTML =
-      (-days-1) + "d " + (-hours-1) + "h " + (-minutes-1) + "m " + (-seconds-1) + "s " + "<br>" +
-      "Since Israel";
-    }
   }
 }
