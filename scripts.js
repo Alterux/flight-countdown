c6eeabfcabf37b118c1830e0e2689821929ff905
Alterux <alterux@gmail.com>
2017-12-22 19:07:38 +0100

    Updated layout and various other changes

--- scripts.js
+++ scripts.js
@@ -1,8 +1,39 @@
+window.onload = function() {
+  // Run countdown.
+  countDown();
+
+  // Update countdown on a 1 sec interval.
+  let x = setInterval(countDown(), 1000);
+
+  $("#monthsCircle").hover(function() {
+    $(this).css("background-color","#553555");
+  }, function() {
+    $(this).css("background-color","#232323");
+  });
+
+  $("#weeksCircle").hover(function() {
+    $(this).css("background-color","#253555");
+  }, function() {
+    $(this).css("background-color","#232323");
+  });
+
+  $("#daysCircle").hover(function() {
+    $(this).css("background-color","#255535");
+  }, function() {
+    $(this).css("background-color","#232323");
+  });
+}
+
+// Are the date circles expanded?
+let dateExpanded = 0;
+
 // Countdown date.
 let countDownDate = new Date("Jun 10, 2018 01:35:00").getTime();
 
-// 1 sec interval.
-let x = setInterval(function() {
+// let color = document.getElementById('monthsCircle').style.backgroundColor;
+
+
+function countDown() {
 
   // Current date.
   let now = new Date().getTime();
@@ -16,30 +47,62 @@ let x = setInterval(function() {
   let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
   let seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
-  let months = Math.round(days / 30 * 10) / 10;
-  let weeks = Math.round(days / 7 * 10) / 10;
+  let months = Math.floor(days / 30);
+  let weeks = Math.floor(days / 7);
 
-  // Display result.
-  document.getElementById('months').innerHTML =
-  months + " Months<br>" +
-  weeks + " Weeks<br>" +
-  days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+  if (dateExpanded === 1) {
 
-  document.getElementById('weeks').innerHTML =
-  months + " Months<br>" +
-  weeks + " Weeks<br>" +
-  days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+    // Display months.
+    document.getElementById('months').innerHTML =
+    months + " Months<br>" +
+    (days % 30 - 1) + " Days<br>" +
+    (hours + 1) + " Hours<br>" +
+    minutes + " Minutes<br>" +
+    seconds + " Seconds";
 
-  document.getElementById('days').innerHTML =
-  months + " Months<br>" +
-  weeks + " Weeks<br>" +
-  days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+    // Display weeks.
+    document.getElementById('weeks').innerHTML =
+    weeks + " Weeks<br>" +
+    (days % 7 + 1) + " Days<br>" +
+    (hours + 1) + " Hours<br>" +
+    minutes + " Minutes<br>" +
+    seconds + " Seconds";
 
-  // After date reached.
-  if (distance < 0) {
-    // clearInterval(x);
-    document.getElementById('countDown').innerHTML =
-    (-days-1) + "d " + (-hours-1) + "h " + (-minutes-1) + "m " + (-seconds-1) + "s " + "<br>" +
-    "Since Israel";
+    // Display days.
+    document.getElementById('days').innerHTML =
+    days + " Days<br>" +
+    (hours + 1) + " Hours<br>" +
+    minutes + " Minutes<br>" +
+    seconds + " Seconds";
+
+    // After date reached.
+    if (distance < 0) {
+      // clearInterval(x);
+      document.getElementById('countDown').innerHTML =
+      (-days-1) + "d " + (-hours-1) + "h " + (-minutes-1) + "m " + (-seconds-1) + "s " + "<br>" +
+      "Since Israel";
+    }
+
+  } else {
+
+    // Display months.
+    document.getElementById('months').innerHTML =
+    months + " Months<br>";
+
+    // Display weeks.
+    document.getElementById('weeks').innerHTML =
+    weeks + " Weeks<br>";
+
+    // Display days.
+    document.getElementById('days').innerHTML =
+    days + " Days<br>";
+
+    // After date reached.
+    if (distance < 0) {
+      // clearInterval(x);
+      document.getElementById('countDown').innerHTML =
+      (-days-1) + "d " + (-hours-1) + "h " + (-minutes-1) + "m " + (-seconds-1) + "s " + "<br>" +
+      "Since Israel";
+    }
   }
-}, 1000);
+}
