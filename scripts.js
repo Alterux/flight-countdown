3de7d0602041275ca43e97a9896d6faa46baab61
Stian Jakobsen <alterux@gmail.com>
2018-07-25 15:53:38 +0300

    Updated timer for Christmas

--- scripts.js
+++ scripts.js
@@ -44,7 +44,7 @@ function daysExpand() {
 }
 
 // Countdown date.
-let countDownDate = new Date("Jun 22, 2018 08:00:00").getTime();
+let countDownDate = new Date("Dec 26, 2018 17:15:00").getTime();
 
 function countDown() {
 
